01dce930177b6836ec233a91d005f404f6b9c1be
Flynn <flynn.whelehan@gmail.com>
2023-12-03 14:05:38 +0000

    Fix single digit case - one digit is the first and last digit!

--- AdventOfCode2023/Day1/Puzzle1.cs
+++ AdventOfCode2023/Day1/Puzzle1.cs
@@ -8,14 +8,7 @@ public class Puzzle1 : IPuzzle
     public void Solve()
     {
         Console.WriteLine("Solving Day 1, Puzzle 1");
-        /*
-            1abc2
-            pqr3stu8vwx
-            a1b2c3d4e5f
-            treb7uchet
 
-            In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142. 
-        */
         var filePath = "";
         var inputLines = FileService.GetFileLines(filePath);
         int problemSum = 0;
@@ -23,30 +16,28 @@ public class Puzzle1 : IPuzzle
         {
             foreach (var input in inputLines)
             {
-                //Console.WriteLine("Input:" + input);
-                //Console.WriteLine("Problem sum so far: " + problemSum);
                 var digits = input.Where(char.IsDigit).ToArray();
-                //Console.WriteLine(digits);
                 if (digits.Length > 0)
                 {
-                    if (digits.Length > 1) 
+                    if (digits.Length > 1)
                     {
                         var firstLastCombination = int.Parse(string.Concat(digits[0], digits[^1]));
                         problemSum += firstLastCombination;
-                        //Console.WriteLine("Added: " + firstLastCombination);
                     }
                     else
                     {
-                        problemSum += digits[0] - '0';
-                        //Console.WriteLine("Added: " + digits[0]);
+                        var singleDigitValue = digits[0] - '0';
+                        problemSum += singleDigitValue * 10 + singleDigitValue;
                     }
-                } else
+                }
+                else
                 {
                     problemSum += 0;
-                    //Console.WriteLine("Added: 0");
                 }
             }
-        } catch (Exception ex)
+            Console.WriteLine("Final value: " + problemSum);
+        }
+        catch (Exception ex)
         {
             Console.WriteLine(ex.ToString());
         }
