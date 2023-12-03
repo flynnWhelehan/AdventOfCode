5ad1f1b822c0b9cbd6eef6dba557991c1451f0f2
Flynn <flynn.whelehan@gmail.com>
2023-12-03 10:54:35 +0000

    Project and puzzle runner setup

---
+++ AdventOfCode2023/AdventOfCode2023/PuzzleRunner.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2023.Interfaces;
+
+namespace AdventOfCode2023;
+
+public static class PuzzleRunner
+{
+    public static void RunPuzzle(string day, string puzzle)
+    {
+        string typeName = $"AdventOfCode.{day}.Puzzle{puzzle}";
+        var puzzleType = Type.GetType(typeName);
+        try { 
+            var puzzleInstance = (IPuzzle)Activator.CreateInstance(puzzleType);
+            puzzleInstance.Solve();
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+    static void Main()
+    {
+        PuzzleRunner.RunPuzzle("Day1", "1");
+    }
+}
